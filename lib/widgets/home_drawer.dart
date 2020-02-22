dfcb6db6953c5a7a33eabd2986212c4a2010ad0e
VincentDo8439 <33644870+VincentDo8439@users.noreply.github.com>
2020-02-22 17:00:33 -0600

    finished profile page and drawer

--- lib/widgets/home_drawer.dart
+++ lib/widgets/home_drawer.dart
@@ -7,19 +7,45 @@ class MainDrawer extends StatelessWidget{
     return SizedBox(
       width: MediaQuery.of(context).size.width * 0.65,
       child: Container(
-        color: Colors.white,
+        color: Colors.yellow.shade200,
         child: Column(
           children: <Widget>[
             SizedBox(
-              height: 100,
+              height: 50,
+            ),
+            Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  CircleAvatar(
+                    radius: 80.0,
+                    backgroundImage: AssetImage("assets/profile.jpg"),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  Text(
+                    'Vincent Do',
+                    style: TextStyle(
+                      fontSize: 25.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15.0,
+                  ),
+                ],
+              ),
             ),
             ListTile(
               leading: Icon(Icons.person_outline),
               title: Text('Profile'),
+              onTap: () {},
             ),
             ListTile(
               leading: Icon(Icons.settings),
               title: Text('Settings'),
+              onTap: () {},
             ),
 
           ],
