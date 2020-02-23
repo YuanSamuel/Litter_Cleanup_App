5cf98c5327c9f2fb367b444d683d7b0a103bf9f3
Samuel Yuan <samuel6161@gmail.com>
2020-02-22 18:31:02 -0600

    update

--- lib/screens/leaderboard.dart
+++ lib/screens/leaderboard.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
-
+import 'camera.dart' as camera;
 class Leaderboard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    camera.PhotoPage p = new camera.PhotoPage();
+    p.createState().getImageFromCamera()
     return Scaffold(
       body: Text(
           'Leaderboard'
